8943624d5c02dd7723ca3841e40d21f758b76324
SATRIABA0506 <satriabagaskaranatural@gmail.com>
2020-08-06 04:43:47 +0700

    Pertemuan 11 Membuat Halaman Template

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -14,7 +14,7 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return view('layouts.app');
+        return view('mahasiswa.index');
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Informasi Akademik</title>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="{{ asset('js/app.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
 </head> 
 <body>
-    @include('layouts.navbar');
-    <h1>Test Halaman Boostrap</h1>
+    @include('layouts.navbar')
+    <div class="container-fluid">
+        
+        <div class="row mt-1">
+            <div class="col-md-2">
+             @include('layouts.sidebar')
+            </div>
+            <div class="col-md-10">
+            @include('layouts.breadcrumd')
+             @yield('content')
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
