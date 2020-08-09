defb1d876bf914f23d2255a7ccda62f21c9055fc
SATRIABA0506 <satriabagaskaranatural@gmail.com>
2020-08-10 04:44:03 +0700

    Pertemuan 11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,21 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+    
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <title>@yield('title')</title>
+    
+    
     
 </head> 
 <body>
     @include('layouts.navbar')
-    <div class="container-fluid">
+    <div class="container">
         
-        <div class="row mt-1">
-            <div class="col-md-2">
+        <div class="row">
+            <div class="col-md-3">
              @include('layouts.sidebar')
             </div>
-            <div class="col-md-10">
-            @include('layouts.breadcrumd')
+            <div class="col-md-9">
              @yield('content')
             </div>
         </div>
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,8 +1,42 @@
 @extends('layouts.app')
-@section('title','Halaman Mahasiswa')
+@section('title','Mahasiswa Page')
 @section('bread1','Mahasiswa')
-@section('bread2','Daftar')
+@section('bread2','Data')
 
 @section('content')
-<p>This is my body content.</p>
+<h1>Master Data Mahasiswa</h1>
+<p><a href="/mhs/create" class="btn btn-success btn-sm">Tambah</a></p>
+    @include('layouts.alert')
+
+<table class="table table-stripped" id="mhs_table">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>NIM</th> 
+            <th>Nama Lengkap</th>
+            <th>Program Studi</th>
+            <th>Alamat</th>
+            <th>Pilihan</th>
+
+        </tr>
+    </thead>
+</table> 
+<script>
+$(function() {
+    $('#mhs-table').DataTable({
+        processing: true,
+            serverSide: true,
+            ajax: "{{ route('mhs.list') }}",
+            columns: [
+               { data: 'DT_RowIndex', name: 'DT_RowIndex' },
+               { data: 'nim', name: 'nim' },
+               { data: 'nama_lengkap', name: 'nama_lengkap' },
+               { data: 'mprodi.nama_prodi', name: 'nama_prodi' },
+               { data: 'alamat', name: 'alamat' },
+               { data: 'action', name: 'action', orderable: false,searchable: false }
+
+        ]
+    });
+});
+</script>
 @endsection
